7b78da6752535b2e5a94149a1c7106015727c1d4
volodymyr.romaniv <v.b.romaniv@gmail.com>
2022-06-10 23:45:08 +0300

    Added deploy script for BikeToken

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const BikeToken = await ethers.getContractFactory("BikeToken");
+    const token = await BikeToken.deploy(1000000);
+
+    console.log("BikeToken address:", token.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
