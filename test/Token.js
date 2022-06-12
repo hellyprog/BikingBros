90125928dfd495866e3be156a2fe9c13391a89f2
volodymyr.romaniv <v.b.romaniv@gmail.com>
2022-06-12 14:07:05 +0300

    Added BIKE initialization unit tests

--- test/Token.js
+++ test/Token.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 
-describe("BikeToken contract", function () {
+describe("BikeToken contract initialization", function () {
   it("Deployment should set initial supply as constructor parameter", async function () {
     const BikeToken = await ethers.getContractFactory("BikeToken");
 
@@ -8,5 +8,32 @@ describe("BikeToken contract", function () {
     const token = await BikeToken.deploy(initialSupply);
 
     expect(await token.totalSupply()).to.equal(initialSupply);
+  });
+
+  it("Should return 'BIKE' as symbol", async function () {
+    const BikeToken = await ethers.getContractFactory("BikeToken");
+
+    const initialSupply = 1000000;
+    const token = await BikeToken.deploy(initialSupply);
+
+    expect(await token.symbol()).to.equal('BIKE');
+  });
+
+  it("Should return 'BikingBros' as name", async function () {
+    const BikeToken = await ethers.getContractFactory("BikeToken");
+
+    const initialSupply = 1000000;
+    const token = await BikeToken.deploy(initialSupply);
+
+    expect(await token.name()).to.equal('BikingBros');
+  });
+
+  it("Should have 18 decimals", async function () {
+    const BikeToken = await ethers.getContractFactory("BikeToken");
+
+    const initialSupply = 1000000;
+    const token = await BikeToken.deploy(initialSupply);
+
+    expect(await token.decimals()).to.equal(18);
   });
 });
