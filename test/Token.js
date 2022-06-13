53ed529fc316b488cc6d6240082b0e01c9c8fd6e
volodymyr.romaniv <v.b.romaniv@gmail.com>
2022-06-13 16:57:04 +0300

    Refactored tests

--- test/Token.js
+++ test/Token.js
@@ -1,39 +1,38 @@
 const { expect } = require("chai");
 
-describe("BikeToken contract initialization", function () {
-  it("Deployment should set initial supply as constructor parameter", async function () {
-    const BikeToken = await ethers.getContractFactory("BikeToken");
+const initialSupply = 1000000;
 
-    const initialSupply = 1000000;
-    const token = await BikeToken.deploy(initialSupply);
+let BikeToken;
+let deployedToken;
+let owner, addr1, addr2, addrs;
 
-    expect(await token.totalSupply()).to.equal(initialSupply);
+beforeEach(async () => {
+  BikeToken = await ethers.getContractFactory("BikeToken");
+  [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+  deployedToken = await BikeToken.deploy(initialSupply);
+});
+
+describe("BikeToken contract initialization", () => {
+  it("Deployment should set initial supply as constructor parameter", async () => {
+    expect(await deployedToken.totalSupply()).to.equal(initialSupply);
   });
 
-  it("Should return 'BIKE' as symbol", async function () {
-    const BikeToken = await ethers.getContractFactory("BikeToken");
-
-    const initialSupply = 1000000;
-    const token = await BikeToken.deploy(initialSupply);
-
-    expect(await token.symbol()).to.equal('BIKE');
+  it("Should return 'BIKE' as symbol", async () => {
+    expect(await deployedToken.symbol()).to.equal('BIKE');
   });
 
-  it("Should return 'BikingBros' as name", async function () {
-    const BikeToken = await ethers.getContractFactory("BikeToken");
-
-    const initialSupply = 1000000;
-    const token = await BikeToken.deploy(initialSupply);
-
-    expect(await token.name()).to.equal('BikingBros');
+  it("Should return 'BikingBros' as name", async () => {
+    expect(await deployedToken.name()).to.equal('BikingBros');
   });
 
-  it("Should have 18 decimals", async function () {
-    const BikeToken = await ethers.getContractFactory("BikeToken");
+  it("Should have 18 decimals", async () => {
+    expect(await deployedToken.decimals()).to.equal(18);
+  });
+});
 
-    const initialSupply = 1000000;
-    const token = await BikeToken.deploy(initialSupply);
-
-    expect(await token.decimals()).to.equal(18);
+describe('BikingBros accounts', () => {
+  it('Owner balance should be 1000000', async () => {
+    expect(await deployedToken.balanceOf(owner.address)).to.equal(1000000);
   });
 });
