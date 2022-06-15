1b77f435806976f77388c57c2f83cb0948d03abd
volodymyr.romaniv <v.b.romaniv@gmail.com>
2022-06-15 23:38:16 +0300

    Added openzeppelin governance

--- contracts/BikeToken.sol
+++ contracts/BikeToken.sol
@@ -1,11 +1,35 @@
 // contracts/BikeToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
 
-contract BikeToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("BikingBros", "BIKE") {
+contract BikeToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor(uint256 initialSupply) ERC20("BikingBros", "BIKE") ERC20Permit("BikingBros") {
         _mint(msg.sender, initialSupply);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
     }
 }
